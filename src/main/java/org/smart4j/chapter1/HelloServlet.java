2e4af974ccda101b8ceec3660b2a77a69b0bec8b
hellojava1233 <choosetruth@163.com>
2018-01-30 13:27:29 +0800

    添加注释

--- src/main/java/org/smart4j/chapter1/HelloServlet.java
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ *  created by YP
+ *   date : 2018.01.30
+ */
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
